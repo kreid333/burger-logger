0e3f669c475c7c33102a2cef074c12afb5d22bcf
kreid333 <kai.j.reid@gmail.com>
2020-10-08 17:31:54 -0400

    completed connection.js and exported it to server.js

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
+// DEFINING VARIABLES
 const exphbs = require("express-handlebars");
+const connection = require("./config/connection");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+
+
+
+
+
+// TURNING ON EXPRESS SERVER AND MYSQL CONNECTION
 app.listen((err) => {
     if (err) throw err;
     console.log(`App listening at http://localhost:${PORT}`);
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Database connected...");
 })
