e0e2afe28aea3f59bf8d2b8b46c3e0d962113c7c
kreid333 <kai.j.reid@gmail.com>
2020-10-08 17:43:27 -0400

    edited app.listen, now works properly

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
 // DEFINING VARIABLES
+const express = require("express");
 const exphbs = require("express-handlebars");
 const connection = require("./config/connection");
 
@@ -7,13 +7,15 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// MIDDLEWARE// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-
-
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // TURNING ON EXPRESS SERVER AND MYSQL CONNECTION
-app.listen((err) => {
+app.listen(PORT, (err) => {
     if (err) throw err;
     console.log(`App listening at http://localhost:${PORT}`);
 })
